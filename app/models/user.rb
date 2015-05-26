ec32ee05bcd3a7531fe1259c8136261a4850e656
Savatia <briankeyonzo@yahoo.com>
2015-05-26 19:01:31 +0300

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,12 +26,10 @@ class User < ActiveRecord::Base
 	end 
 
 	#returns true if the token matches the digest
-	def authenticated?(remember_token)
-		if remember_digest.nil?
-			return false
-		else
-			BCrypt::Password.new(remember_digest).is_password?(remember_token)
-		end
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	def forget
@@ -47,5 +45,14 @@ class User < ActiveRecord::Base
     def create_activation_digest
     	self.activation_token = User.new_token
     	self.activation_digest = User.digest(activation_token)
+    end
+
+    def activate
+    	update_attribute(:activated, true)
+    	update_attribute(:activated_at, Time.zone.now)
+    end
+
+    def send_activation_email
+    	UserMailer.account_activation(self).deliver_now
     end
 end
